40b14bd7ff0a941533048d5997e2b194f17bca74
psandhya8 <sandhyapachupate8010@gmail.com>
2020-12-17 02:48:40 +0530

    Alignment Changed

--- display-category.js
+++ display-category.js
@@ -6,15 +6,24 @@ const categoryDisplay = document.getElementById('categoryDisplay');
 categoryCollection.onSnapshot(function(querySnapshot) {
     
         // doc.data() is never undefined for query doc snapshots
-        querySnapshot.docChanges().forEach(function(change){
+        querySnapshot.docChanges().forEach(function(change,i){
             if(change.type === "added"){
-                  categoryDisplay.innerHTML += "<div class = 'card-body'><h4>" + change.doc.data().categoryName + 
-                  "</h4><p>" +change.doc.data().categoryShortname+"</p><p class='card-text text-muted'>" 
-                  +change.doc.data().categoryDescription +"</p></div>"
+                if(i === 0){
+                  categoryDisplay.innerHTML += "<div class ='row'>"
+                }
+                categoryDisplay.innerHTML += "<div class='col-lg-6'><div class= 'card'><div class = 'card-body'><h4>" + change.doc.data().categoryName + 
+                "</h4><p>" +change.doc.data().categoryShortname+"</p><p class='card-text text-muted'>" 
+                +change.doc.data().categoryDescription +"</p></div></div>"
+                if(i!=0 && i%2 == 0){
+
+                    // add end of row ,and start new row on every 5 elements
+                    categoryDisplay.innerHTML += '</div>'
+                  }
             }
 
         });
-      
+        categoryDisplay.innerHTML += '</div>'
+        
 
         
 });
